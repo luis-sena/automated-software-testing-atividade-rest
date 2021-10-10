a320c3e9ba787ddd80ae862904c2c49e4fc0f7ef
Luis Sena <luisfj.sena@gmail.com>
2021-10-10 13:23:09 -0300

    Atualizando README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Atividade Rest
+
+Aula 1 "ESD21 - Automated Software Testing" - Atividade Rest
+
+## Desafio
+
+> O componente deve receber os exemplos acima e retornar verdadeiro ou falso, conforme a
+> regra. Fazer o componente usando a metodologia TDD.
+> Enviar a URL do Github com todo o código fonte.
+> Fonte: https://en.wikipedia.org/wiki/Palindrome
+
+## Como rodar os testes
+
+```sh
+git clone https://github.com/luis-sena/automated-software-testing-atividade-rest.git && cd automated-software-testing-atividade-rest
+
+gradle clean test
+```
+![](tests.png)
+
--- src/test/java/br/com/lsena/api/ApiRestTest.java
+++ src/test/java/br/com/lsena/api/ApiRestTest.java
@@ -2,6 +2,7 @@ package br.com.lsena.api;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
@@ -15,12 +16,14 @@ public class ApiRestTest {
     }
 
     @Test
+    @DisplayName("Should Return StatusCode 200 And Nu Pagamentos IP")
     public void shouldReturnStatusCode200AndNuPagamentosIP() {
         String bankName = get("/banks/v1/260").then().statusCode(200).extract().path("name");
         assertEquals("NU PAGAMENTOS - IP", bankName);
     }
 
     @Test
+    @DisplayName("Should Return StatusCode 404")
     public void shouldReturnStatusCode404() {
         get("/banks/v1/2").then().statusCode(404);
     }
